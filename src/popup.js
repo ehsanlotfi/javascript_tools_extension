c418b0559b7a07fe32415ca86eb9f2b7ea2bc2fa
ehsanlotfi <lotfiniamail@gmail.com>
2018-04-12 15:49:19 +0430

    first commit

---
+++ src/popup.js
@@ -0,0 +1,18 @@
+$(function() {
+    var url = [
+        "https://api.github.com/repos/nodejs/Node/releases/latest",
+        "https://api.github.com/repos/Microsoft/TypeScript/releases/latest",
+        "https://api.github.com/repos/facebook/React/releases/latest",
+        "https://api.github.com/repos/facebook/React-native/releases/latest",
+        "https://api.github.com/repos/webpack/Webpack/releases/latest",
+        "https://api.github.com/repos/vuejs/Vue/releases/latest"
+    ];
+
+    url.forEach(f => {
+        $.when($.ajax(f)).then(d => {
+            $("ul").append(`<li><a target="_blank" href="${d.html_url}">${f.split("/")[5]} : <span>${d.tag_name.replace("v","")}</span></a></li>`)
+        });
+    })
+
+
+});
