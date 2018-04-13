2e28d5ee14d0a09c2306de3eed8d00000d91326a
ehsanlotfi <lotfiniamail@gmail.com>
2018-04-13 11:39:32 +0430

    desian template

--- src/popup.js
+++ src/popup.js
@@ -1,19 +1,19 @@
 $(function() {
-    var url = [
-        "https://api.github.com/repos/nodejs/Node/releases/latest",
-        "https://api.github.com/repos/Microsoft/TypeScript/releases/latest",
-        "https://api.github.com/repos/facebook/React/releases/latest",
-        "https://api.github.com/repos/facebook/React-native/releases/latest",
-        "https://api.github.com/repos/webpack/Webpack/releases/latest",
-        "https://api.github.com/repos/vuejs/Vue/releases/latest",
-        "https://api.github.com/repos/d3/D3/releases/latest"
-    ];
+    // var url = [
+    //     "https://api.github.com/repos/nodejs/Node/releases/latest",
+    //     "https://api.github.com/repos/Microsoft/TypeScript/releases/latest",
+    //     "https://api.github.com/repos/facebook/React/releases/latest",
+    //     "https://api.github.com/repos/facebook/React-native/releases/latest",
+    //     "https://api.github.com/repos/webpack/Webpack/releases/latest",
+    //     "https://api.github.com/repos/vuejs/Vue/releases/latest",
+    //     "https://api.github.com/repos/d3/D3/releases/latest"
+    // ];
 
-    url.forEach(f => {
-        $.when($.ajax(f)).then(d => {
-            $("ul").append(`<li><a target="_blank" href="${d.html_url}">${f.split("/")[5]} : <span>${d.tag_name.replace("v","")}</span></a></li>`)
-        });
-    })
+    // url.forEach(f => {
+    //     $.when($.ajax(f)).then(d => {
+    //         $("ul").append(`<li><a target="_blank" href="${d.html_url}">${f.split("/")[5]} : <span>${d.tag_name.replace("v","")}</span></a></li>`)
+    //     });
+    // })
 
 
 });
